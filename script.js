edd3925be8ba3ae66fb15b02d341bf3fb402f86f
Biediger <cabiediger@gmail.com>
2023-09-22 15:39:00 -0400

    update cors

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const app = express();
 // const env = require('./environment')
 const env = process.env
 const PORT = process.env.PORT || 3001;
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 // Connect to db
 const knex = require('knex')({
@@ -27,11 +27,11 @@ const knex = require('knex')({
 });
 
 app.use(express.json())
-// app.use(cors());
-app.use(cors({
-  origin: 'https://cbied.github.io',
-  methods: ['GET', 'POST', 'PUT']
-}))
+app.use(cors());
+// app.use(cors({
+//   origin: 'https://cbied.github.io',
+//   methods: ['GET', 'POST', 'PUT']
+// }))
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "https://cbied.github.io");

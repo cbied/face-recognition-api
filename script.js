4df9fcdd55a0d18f62363d8942cec2eaebe04240
Biediger <cabiediger@gmail.com>
2023-09-22 08:56:50 -0400

    update headers for cors

--- script.js
+++ script.js
@@ -25,7 +25,14 @@ const knex = require('knex')({
 });
 
 app.use(express.json())
-app.use(cors({ origin: "https://cbied.github.io/", credentials: true }))
+app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 // GET
 
@@ -58,4 +65,4 @@ app.put('/image', (req, res) => { image.handleImage(req,res,knex) })
 ////////////////////////////////////////////////////////////////////////////
 
 // LISTEN 
-app.listen(PORT, () => console.log(`Workin' Hard on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Working on port ${PORT}!`))

e97ac665c1fb7a0e6cda28e885d142b58a4186f6
Biediger <cabiediger@gmail.com>
2023-09-22 15:05:03 -0400

    update env

--- environment.js
+++ environment.js
@@ -1,2 +1,8 @@
+module.exports = {
+    herokuDatabase: 'del3ipr7j28aqc',
+    herokuDbPassword: '0eda83351b66e0b9f10e8b666700bba069cce4a6aebd34f1638140c6bfcd6d5d',
+    herokuUser: 'ihdrlzvaerzkvm',
+    herokuHost: 'ec2-54-243-32-226.compute-1.amazonaws.com'
+}
 
 

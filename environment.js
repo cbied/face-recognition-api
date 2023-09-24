e200aee21345f94f18fc481337c26be5bdcfed54
Biediger <cabiediger@gmail.com>
2023-09-24 15:14:16 -0400

    update heroku configs

--- environment.js
+++ environment.js
@@ -1,2 +1,6 @@
-
-
+module.exports = {
+    herokuDatabase: 'del3ipr7j28aqc',
+    herokuDbPassword: '0eda83351b66e0b9f10e8b666700bba069cce4a6aebd34f1638140c6bfcd6d5d',
+    herokuHost: 'ec2-54-243-32-226.compute-1.amazonaws.com',
+    herokuUser:'ihdrlzvaerzkvm'
+}

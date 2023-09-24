b26b76662527b5bb65d4bd782d3e0a64b434f335
Biediger <cabiediger@gmail.com>
2023-09-24 15:14:16 -0400

    update heroku configs

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const app = express();
-// const env = require('./environment')
-const env = process.env
+const env = require('./environment')
+// const env = process.env
 const PORT = process.env.PORT || 3001;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -27,17 +27,17 @@ const knex = require('knex')({
 });
 
 app.use(express.json())
-// app.use(cors());
-app.use(cors({
-  origin: 'https://cbied.github.io',
-  methods: ['GET', 'POST', 'PUT']
-}))
+app.use(cors());
+// app.use(cors({
+//   origin: 'https://cbied.github.io',
+//   methods: ['GET', 'POST', 'PUT']
+// }))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cbied.github.io");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://cbied.github.io");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // GET
 
